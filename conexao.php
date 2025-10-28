ce5d43699a05ccda65bb99142df1c3e7d3281d66
João Naziozeno <joaonazi@gmail.com>
2025-10-27 21:18:09 -0300

    Migração feita porem esta duplicando

--- conexao.php
+++ conexao.php
@@ -25,5 +25,29 @@ if ($resultado->num_rows > 0) {
     print_r($dadosVeiculos);
     echo "</pre>";
 }
+foreach ($dadosVeiculos as $veiculo) {
+    $id = $veiculo['id'];
+    $marca = $veiculo['marca'];
+    $modelo = $veiculo['modelo'];
+    $ano = $veiculo['ano'];
+    $quilometragem = $veiculo['quilometragem'];
+    $preco = $veiculo['preco'];
+    $cor = $veiculo['cor'];
+    $combustivel = $veiculo['combustivel'];
+    $cambio = $veiculo['cambio'];
+    $portas = $veiculo['portas'];
+    $observacoes = $veiculo['observacoes'];
+    $data_cadastro = $veiculo['data_cadastro'];
+
+    $sqlInsert = "INSERT INTO veiculos2 (marca, modelo, ano, quilometragem, preco, cor, combustivel, cambio, portas, observacoes, data_cadastro) VALUES ('$marca', '$modelo', '$ano', '$quilometragem', '$preco', '$cor', '$combustivel', '$cambio', '$portas', '$observacoes', '$data_cadastro')";
+    $conexaoDestino->query($sqlInsert);
+
+    if ($conexaoDestino->query($sqlInsert) === TRUE) {
+        echo "Registro inserido com sucesso na tabela destino veiculos. <br>";
+    } else {
+        echo "Erro ao inserir registro na tabela destino veiculos: " . $conexaoDestino->error . "<br>";
+    }
+}
+$conexaoOrigem->close();
 
 ?>
